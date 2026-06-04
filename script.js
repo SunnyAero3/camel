0a739ec4511b35573ceac24a0dc6c76fe99f5f56
SunnyAero3 <jake.hili3231@gmail.com>
2026-06-04 14:26:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const counter = document.getElementById("counter");
 const windowArea = document.getElementById("windowArea");
 const sound = document.getElementById("sound");
 
+// 🐫 camel button
 btn.onclick = () => {
   spawnCamel();
+
   count++;
   counter.innerText = "Camels: " + count;
 
@@ -17,25 +19,36 @@ btn.onclick = () => {
   }
 };
 
+// 🐫 clean camel spawn
 function spawnCamel() {
   const img = document.createElement("img");
   img.src = "camel.png";
+  img.style.width = "60px";
+
   area.appendChild(img);
 }
 
+// 💀 activate virus mode
 function activateVirusMode() {
   virusMode = true;
 
+  // 🔊 stable sound
+  sound.volume = 0.6;
+  sound.loop = true;
   sound.play();
 
+  // 🪟 start spawning windows
   setInterval(() => {
     spawnWindow();
   }, 700);
 }
 
+// 🪟 window spawn (draggable + image)
 function spawnWindow() {
   const win = document.createElement("div");
   win.className = "window active";
+
+  win.style.position = "absolute";
   win.style.top = Math.random() * 80 + "%";
   win.style.left = Math.random() * 80 + "%";
 
@@ -46,13 +59,16 @@ function spawnWindow() {
         <button aria-label="Close"></button>
       </div>
     </div>
+
     <div class="window-body">
-      <p>🐫 camel.exe has taken over</p>
+      <img src="camel.png" style="width:120px; display:block; margin:auto;" />
+      <p style="text-align:center;">camel.exe has taken over</p>
     </div>
   `;
 
   const closeBtn = win.querySelector("button");
 
+  // 💥 close = spawn 6 more windows
   closeBtn.onclick = () => {
     win.remove();
 
@@ -62,4 +78,38 @@ function spawnWindow() {
   };
 
   windowArea.appendChild(win);
+
+  enableDrag(win);
+}
+
+// 🖱️ draggable windows (title bar only)
+function enableDrag(el) {
+  const titleBar = el.querySelector(".title-bar");
+
+  let isDown = false;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  titleBar.style.cursor = "grab";
+
+  titleBar.onmousedown = (e) => {
+    isDown = true;
+
+    offsetX = e.clientX - el.offsetLeft;
+    offsetY = e.clientY - el.offsetTop;
+
+    titleBar.style.cursor = "grabbing";
+  };
+
+  document.onmouseup = () => {
+    isDown = false;
+    titleBar.style.cursor = "grab";
+  };
+
+  document.onmousemove = (e) => {
+    if (!isDown) return;
+
+    el.style.left = (e.clientX - offsetX) + "px";
+    el.style.top = (e.clientY - offsetY) + "px";
+  };
 }
